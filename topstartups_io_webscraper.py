7d0c938c25f4ec62430f411039cf7dcc782aac05
Lily Jiang <ljiang@olin.edu>
2022-03-28 13:17:42 -0400

    add notebook file, change scraper to diff website

--- topstartups_io_webscraper.py
+++ topstartups_io_webscraper.py
@@ -1,58 +1,26 @@
-from ast import Num
-import numbers
 from bs4 import BeautifulSoup
 import requests
 
+html_text = requests.get('https://www.failory.com/startups/mobile-apps- \
+    unicorns%27').text
+soup = BeautifulSoup(html_text, 'lxml')
+company_infos = soup.find_all('div', class__ = 'listicle-startup-collection- \
+    item w-dyn-item')
+company_info = []
 
-def find_data_for_an_industry(industry):
-    '''
-    Scrapes the website topstartups.io for company data for a specific industry.
+file = open("topstartups_data.txt", "w")
+for info in company_infos:
+    name = info.find('h3', class__ = 'listicle-h3 content-h3').text
+    file.write(name)
 
-    Specifically, the name of the company, industry type, and seed/series funding
-    are recorded.
-    '''
+    info_box = info.find_all('li', class__ = 'listicle-list-item')
+    info_boxes = []
+    for box in info_box:
+        box_as_string = str(box.text)
+        info_boxes.append(box_as_string)
 
-    html_text = requests.get('https://topstartups.io').text
-    soup = BeautifulSoup(html_text, 'lxml')
-    text_boxes = soup.find_all('div', id = 'item-card')
+    company_info.append(info_boxes)
+    file.write(info_boxes)
 
-
-    file = open("topstartups_data.txt", "w")
-    for text_box in text_boxes:
-        title = text_box.find('a', id = 'startup-website-link').text
-        
-        industry_tag = text_box.find_all('span', id = 'industry-tags')
-        industry_tags = []
-        for tag in industry_tag:
-            tag_as_string = str(tag.text)
-            if len(tag_as_string) == 0:
-                continue
-            elif tag_as_string[0] == " ":
-                tag_as_string = tag_as_string[1:]
-            industry_tags.append(tag_as_string)
-
-        funding_amount = text_box.find_all('span', id = 'funding-tags')
-        funding_by_series = []
-        for amount in funding_amount:
-            amount_as_string = str(amount.text)
-            if len(amount_as_string) == 0:
-                continue
-            elif amount_as_string[0] == " ":
-                amount_as_string = amount_as_string[1:]
-            
-            # filter amounts by seeds/series here
-            if "Series" in amount_as_string:
-                series_index = amount_as_string.index("Series")
-                series_type = amount_as_string[series_index : series_index+8]
-                series_value = amount_as_string.split(" ", 1)[0]
-                funding_by_series.append((series_type, series_value))
-            elif "Seed" in amount_as_string:
-                series_value = amount_as_string.split(" ", 1)[0]
-                funding_by_series.append(("Seed", series_value))
-
-        # overwrite the contents of the file with the new data
-        if industry in industry_tags:
-            file.write(f"Company Name: {title}\n")
-            file.write(f"Industry Type: {industry_tags}\n")
-            file.write(f"Funding by series: {funding_by_series}\n")
-    file.close()
+print(company_info)
+file.close()
